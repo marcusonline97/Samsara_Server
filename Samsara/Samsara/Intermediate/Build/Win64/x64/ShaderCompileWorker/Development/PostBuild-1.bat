f7f2365425007e549a88528c6c4e2b9b89475fd7
marcusonline97 <Mjpersson97@gmail.com>
2025-06-14 15:43:09 +0200

    Reset

--- Samsara/Samsara/Intermediate/Build/Win64/x64/ShaderCompileWorker/Development/PostBuild-1.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo Copying D:\UnrealEngine\UnrealEngine\UnrealEngine\Engine\Binaries\Win64\ShaderCompileWorker.exe to D:\UnrealEngine\UnrealEngine\UnrealEngine\Engine\Binaries\Win64\XGEControlWorker.exe
-copy /Y /B "D:\UnrealEngine\UnrealEngine\UnrealEngine\Engine\Binaries\Win64\ShaderCompileWorker.exe" /B "D:\UnrealEngine\UnrealEngine\UnrealEngine\Engine\Binaries\Win64\XGEControlWorker.exe" >nul:
